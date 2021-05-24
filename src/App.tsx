5ed0768fffd6603c9c66e282686dea8ccea90f17
Darcio Vilela <darcio@jp7.com.br>
2021-05-24 19:36:30 -0300

    Add movies list

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import { MoviesList } from './components/MoviesList'
 
 function App() {
   return (
@@ -19,6 +20,7 @@ function App() {
           Learn React
         </a>
       </header>
+      <MoviesList />
     </div>
   );
 }
---
+++ src/components/MoviesList.tsx
@@ -0,0 +1,14 @@
+export const MoviesList = () => {
+  return (
+    <div>
+      <h2>Movies List:</h2>
+      <table>
+        <tr>
+          <td>Movie</td>
+          <td>Director</td>
+          <td>Released</td>
+        </tr>
+      </table>
+    </div>
+  )
+}
