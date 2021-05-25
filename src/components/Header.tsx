bda78b9f156a90f0055f54529ee3ad22de852fee
Darcio Vilela <darcio@jp7.com.br>
2021-05-25 18:56:03 -0300

    Custom header

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,20 +1,13 @@
-import logo from '../logo.svg';
+import logo from '../logo_movie.png';
 
 export const Header = () => {
   return (
     <header className="App-header">
       <img src={logo} className="App-logo" alt="logo" />
       <p>
-        Edit <code>src/App.tsx</code> and save to reload.
+        <b>All kind of movies. Old and new classics.</b> <br />
+        Lots seen more than once.
       </p>
-      <a
-        className="App-link"
-        href="https://reactjs.org"
-        target="_blank"
-        rel="noopener noreferrer"
-      >
-        Learn React
-      </a>
     </header>
   );
 };
