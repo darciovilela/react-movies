79b5e2f9097aaafa709bed13de35b4c0c23aed68
Darcio Vilela <darcio@jp7.com.br>
2021-05-26 18:58:57 -0300

    Add footer to the site

--- src/App.tsx
+++ src/App.tsx
@@ -2,12 +2,14 @@ import React from 'react';
 import './App.css';
 import { Header } from './components/Header';
 import { MoviesList } from './components/MoviesList';
+import { Footer } from './components/Footer';
 
 function App() {
   return (
     <div className="App">
       <Header />
       <MoviesList />
+      <Footer />
     </div>
   );
 }
---
+++ src/components/Footer.tsx
@@ -0,0 +1,12 @@
+export const Footer = () => {
+  return (
+    <footer className="App-footer">
+      <div>
+        <p>
+          <b>&#169; Copyright 2021</b> - "Watch a movie a day and enjoy the
+          moment"
+        </p>
+      </div>
+    </footer>
+  );
+};
