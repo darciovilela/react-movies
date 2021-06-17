1bb52f64f53a01c7a64d2db11b150b708e3a77f4
JP Barbosa <jp@jp7.com.br>
2021-06-17 16:11:46 -0300

    Use axios to fetch movies

--- src/components/MoviesList.tsx
+++ src/components/MoviesList.tsx
@@ -1,7 +1,28 @@
-import db from '../data/db.json';
-const movies = db.movies;
+import { useState, useEffect } from 'react';
+import axios from 'axios';
+
+interface Movie {
+  name: string;
+  director: string;
+  released: string;
+}
 
 export const MoviesList = () => {
+  const [movies, setMovies] = useState<Movie[]>([]);
+
+  // componentDidMount
+  useEffect(() => {
+    const callFetchFunction = async () => {
+      const result = await axios.get<Movie[]>('http://localhost:4000/movies');
+      setMovies(result.data);
+    };
+    callFetchFunction();
+  }, []);
+
+  if (!movies.length) {
+    return <div>Loading... (or empty)</div>;
+  }
+
   return (
     <div>
       <h2>My {movies.length} Favorite Movies:</h2>
