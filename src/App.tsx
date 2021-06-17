5d7d30ef27afc2f64796f1b113b60dd904080838
JP Barbosa <jp@jp7.com.br>
2021-06-17 17:00:15 -0300

    Use state to set current page

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './App.css';
 import { Header } from './components/Header';
 import { MoviesList } from './components/MoviesList';
@@ -8,13 +8,25 @@ import { LastSeen } from './components/LastSeen';
 import { LatinMovies } from './components/LatinMovies';
 
 function App() {
+  const [page, setPage] = useState('Movies');
+
+  const swithPage = () => {
+    switch (page) {
+      case 'Movies':
+        return <MoviesList />;
+      case 'Actors':
+        return <ActorsList />;
+      case 'Last Seen':
+        return <LastSeen />;
+      case 'Latin Movies':
+        return <LatinMovies />;
+    }
+  };
+
   return (
     <div className="App">
-      <Header />
-      <MoviesList />
-      <ActorsList />
-      <LastSeen />
-      <LatinMovies />
+      <Header setPage={setPage} />
+      {swithPage()}
       <Footer />
     </div>
   );
--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,6 +1,10 @@
 import logo from '../img/new_logo_movie.png';
 
-export const Header = () => {
+interface IProps {
+  setPage: Function;
+}
+
+export const Header = (props: IProps) => {
   return (
     <header className="App-header">
       <img src={logo} className="App-logo" alt="logo" />
@@ -8,6 +12,14 @@ export const Header = () => {
         <b>All kind of movies. Old and new classics.</b> <br />
         Lots seen more than once.
       </p>
+      <div>
+        <ul>
+          <li onClick={() => props.setPage('Movies')}>Movies</li>
+          <li onClick={() => props.setPage('Actors')}>Actors</li>
+          <li onClick={() => props.setPage('Last Seen')}>Last Seen</li>
+          <li onClick={() => props.setPage('Latin Movies')}>Latin Movies</li>
+        </ul>
+      </div>
     </header>
   );
 };
